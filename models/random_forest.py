199e437500499118eaa807ccff1db3ba3802913a
OuniAchir <153436709+OuniAchir@users.noreply.github.com>
2025-01-06 23:01:36 +0100

    Update random_forest.py

--- models/random_forest.py
+++ models/random_forest.py
@@ -2,6 +2,19 @@ from pyspark.ml.classification import RandomForestClassifier
 from pyspark.ml.evaluation import MulticlassClassificationEvaluator
 
 def train_random_forest(train_df, selected_tags):
+    """
+    Cette fonction entraîne des modèles de forêts aléatoires (Random Forest) pour chaque étiquette spécifiée
+    et retourne les modèles entraînés ainsi que les résultats d'évaluation accuracy et F1-score
+
+    Arguments 
+    - train_df : DataFrame Spark contenant les données d'entraînement.
+    - selected_tags : Liste des étiquettes (tags) pour lesquelles les modèles seront entraînés
+
+    Retourne 
+    - models : Dictionnaire contenant les modèles de forêts aléatoires pour chaque étiquette.
+    - results : Dictionnaire contenant les métriques d'évaluation accuracy et F1-score pour chaque étiquette
+    """
+    
     models = {}
     results = {}
     for tag in selected_tags:
