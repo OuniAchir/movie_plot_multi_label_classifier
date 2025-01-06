313e130e8d7890c0353141c85560d2275eded81a
OuniAchir <153436709+OuniAchir@users.noreply.github.com>
2025-01-06 23:04:41 +0100

    Update train_svm.py

--- models/train_svm.py
+++ models/train_svm.py
@@ -2,6 +2,19 @@ from pyspark.ml.classification import LinearSVC
 from pyspark.ml.evaluation import MulticlassClassificationEvaluator
 
 def train_svm(train_df, selected_tags):
+    """
+    Cette fonction entraîne des modèles SVM linéaires pour chaque étiquette spécifiée
+    et retourne les modèles entraînés ainsi que les résultats d'évaluation accuracy
+
+    Arguments
+    - train_df : DataFrame Spark contenant les données d'entraînement
+    - selected_tags : Liste des étiquettes (tags) pour lesquelles les modèles seront entraînés
+
+    Retourne
+    - model_svm : Dictionnaire contenant les modèles Linear SVM pour chaque étiquette
+    - results_svm : Dictionnaire contenant les métriques d'évaluation accuracy pour chaque étiquette
+    """
+    
     model_svm = {}
     results_svm = {}
 
