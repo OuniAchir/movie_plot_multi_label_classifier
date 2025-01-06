220ab3735afb45a177f584350f581eac8c9b62ec
OuniAchir <153436709+OuniAchir@users.noreply.github.com>
2025-01-06 23:00:17 +0100

    Update logistic_regression.py

--- models/logistic_regression.py
+++ models/logistic_regression.py
@@ -2,6 +2,19 @@ from pyspark.ml.classification import LogisticRegression
 from pyspark.ml.evaluation import MulticlassClassificationEvaluator
 
 def train_logistic_regression(train_df, selected_tags):
+     """
+    Cette fonction entraîne des modèles de régression logistique pour chaque étiquette spécifiée
+    et retourne les modèles entraînés ainsi que les résultats d'évaluation accuracy et F1-score
+
+    Arguments 
+    - train_df : DataFrame Spark contenant les données d'entraînement.
+    - selected_tags : Liste des étiquettes (tags) pour lesquelles les modèles seront entraînés
+
+    Retourne 
+    - models : Dictionnaire contenant les modèles de régression logistique pour chaque étiquette.
+    - results : Dictionnaire contenant les métriques d'évaluation accuracy et F1-score pour chaque étiquette
+    """
+    
     models = {}
     results = {}
     for tag in selected_tags:
