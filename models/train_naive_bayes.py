df7ba1a17295d8d12aed0dedfdd4e575bc3807ab
OuniAchir <153436709+OuniAchir@users.noreply.github.com>
2025-01-06 23:03:15 +0100

    Update train_naive_bayes.py

--- models/train_naive_bayes.py
+++ models/train_naive_bayes.py
@@ -2,6 +2,19 @@ from pyspark.ml.classification import NaiveBayes
 from pyspark.ml.evaluation import MulticlassClassificationEvaluator
 
 def train_naive_bayes(train_df, selected_tags):
+    """
+    Cette fonction entraîne des modèles de Naive Bayes pour chaque étiquette spécifiée
+    et retourne les modèles entraînés ainsi que les résultats d'évaluation (accuracy).
+
+    Arguments :
+    - train_df : DataFrame Spark contenant les données d'entraînement.
+    - selected_tags : Liste des étiquettes (tags) pour lesquelles les modèles seront entraînés.
+
+    Retourne :
+    - model_nb : Dictionnaire contenant les modèles Naive Bayes pour chaque étiquette.
+    - results_nb : Dictionnaire contenant les métriques d'évaluation (accuracy) pour chaque étiquette.
+    """
+    
     model_nb = {}
     results_nb = {}
 
