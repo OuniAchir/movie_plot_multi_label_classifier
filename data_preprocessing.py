cbc9571adb06d71600924d1a8571e28a184904dc
OuniAchir <153436709+OuniAchir@users.noreply.github.com>
2025-01-06 22:57:25 +0100

    Update data_preprocessing.py

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -3,7 +3,9 @@ from pyspark.sql.functions import col, explode, split
 from pyspark.ml import Pipeline
 from pyspark.ml.feature import Tokenizer, StopWordsRemover, HashingTF
 
+# Fonction pour initialiser une session Spark
 def initialize_spark(app_name="MovieML"):
+    # Création de la session Spark avec une configuration spécifique (mémoire, cœurs, etc.)
     spark = SparkSession.builder \
         .appName(app_name) \
         .config("spark.executor.memory", "4g") \
@@ -13,6 +15,7 @@ def initialize_spark(app_name="MovieML"):
     return spark
 
 def load_and_preprocess_data(spark, file_path):
+    # Chargement du fichier CSV avec des options spécifiques pour gérer les lignes multi-lignes, les guillemets.. 
     df = spark.read.option("multiLine", "true") \
         .option("quote", "\"") \
         .option("escape", "\"") \
@@ -20,26 +23,32 @@ def load_and_preprocess_data(spark, file_path):
         .csv(file_path, inferSchema=True, header=True)
     
     df = df.where(col('imdb_id').like("tt%"))
-    # Filter rows with valid IMDb IDs
+    # Filter les lignes pars imbd valide
     df = df.where(col('imdb_id').like("tt%"))
     
-    # Lowercase the text, remove punctuation, and special characters
+    # Mettre tout le texte en minuscule et supprimer la ponctuation et les caractères spéciaux
     df = df.withColumn('plot_synopsis', lower(col('plot_synopsis')))
     df = df.withColumn('plot_synopsis', regexp_replace(col('plot_synopsis'), r'[^a-z\s]', ''))
     
-    # Split the tags into individual rows
+    # Diviser la colonne "tags" en plusieurs lignes, chaque ligne contenant un tag unique
     df = df.withColumn('tags_split', explode(split('tags', ', ')))
     return df
 
+# Fonction pour encoder les données en utilisant Tokenizer, StopWordsRemover et HashingTF
 def encode_data(df, selected_tags):    
+    # Tokenization : séparer le texte en mots individuels
     tokenizer = Tokenizer(inputCol='plot_synopsis', outputCol='words')
     remover = StopWordsRemover(inputCol="words", outputCol="filtered")
+        
+    # Conversion des mots en vecteurs numériques
     hashingTF = HashingTF(inputCol=remover.getOutputCol(), outputCol="features")
-    
+
+    # Transformation des données d'entrée en utilisant le pipeline
     pipeline = Pipeline(stages=[tokenizer, remover, hashingTF])
     pipeline_model = pipeline.fit(df)
     encoded_df = pipeline_model.transform(df)
-    
+
+    # Création d'une table pivot où chaque tag devient une colonne
     pivot_df = encoded_df.groupby('plot_synopsis', 'features', 'split') \
         .pivot('tags_split', selected_tags).count().fillna(0)
     return pivot_df
